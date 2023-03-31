c18638b527aa3dff6d4cb25913ba61bcb97b9de4
Andriy Voznyuk <andreyv@ukr.net>
2023-03-31 18:17:57 +0200

    Revert "dev deploy"
    This reverts commit ee10e616a0f8a1a054f67161d828198797792885.

--- landing/core/settings/base.py
+++ landing/core/settings/base.py
@@ -167,4 +167,4 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "https://a1.ukrainecontentclub.com.ua/"
+WAGTAILADMIN_BASE_URL = "https://ukrainecontentclub.com.ua/"
