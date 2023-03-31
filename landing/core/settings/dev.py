e472e9d7dd1b85ead2132599284358936d3fad7b
Andriy Voznyuk <andreyv@ukr.net>
2023-03-31 18:17:16 +0200

    Revert "test"
    This reverts commit 566b027cddf999073dad2feb754491bfacca2d91.

--- landing/core/settings/dev.py
+++ landing/core/settings/dev.py
@@ -6,21 +6,13 @@ from django.core.management import utils
 DEBUG = True
 print('DEBUG.dev= ', DEBUG)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "dm*#_9rs0r0z$g)65#m12y6wxl@rpw$%1dxu@+(x^dm*#_9rs0r0z$g)65#m12y6wv!=2b%"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME":   os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# SECURITY WARNING: define the correct hosts in production!
+
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-
-DEBUG404 = False
 
 try:
     from .local import *
