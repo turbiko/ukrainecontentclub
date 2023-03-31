ee10e616a0f8a1a054f67161d828198797792885
Andriy Voznyuk <andreyv@ukr.net>
2023-03-31 17:01:04 +0200

    dev deploy

--- landing/core/settings/production.py
+++ landing/core/settings/production.py
@@ -2,7 +2,8 @@ from .base import *
 from django.core.management import utils
 
 DEBUG = False
-
+ALLOWED_HOSTS = ['https://a1.ukrainecontentclub.com.ua/', 'https://ukrainecontentclub.com.ua/']
+CSRF_TRUSTED_ORIGINS = ['https://a1.ukrainecontentclub.com.ua/', 'https://ukrainecontentclub.com.ua/']
 SECRET_KEY = os.path.dirname(PROJECT_DIR)
 
 try:
