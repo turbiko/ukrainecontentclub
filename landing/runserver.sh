ca8efee89a8a08d81e41c8183f434b8c9dc0309e
Andriy Voznyuk <andreyv@ukr.net>
2023-03-31 19:02:40 +0200

    deploy

--- landing/runserver.sh
+++ landing/runserver.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ "${DEBUG}" == "true" ]]; then
-  python manage.py runserver 0.0.0.0:8081
-else
-  daphne -b 0.0.0.0 -p 8081 project.asgi:application
-fi
+python manage.py runserver 0.0.0.0:8081
