4a0501cb5dca1e0462d3243e5913f37f5fcbe86e
Andriy Voznyuk <andreyv@ukr.net>
2023-03-31 18:17:26 +0200

    Revert "Update runserver.sh"
    This reverts commit ea0a7322a1d4ba91e180c89a18e47c3b21137c0e.

--- landing/runserver.sh
+++ landing/runserver.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations --settings=core.settings.dev
 python manage.py migrate --settings=core.settings.dev
 python manage.py collectstatic --settings=core.settings.dev --no-input --clear
 #python manage.py update_index --settings=core.settings.dev
-gunicorn core.wsgi:application -b :8081  --workers=5   --timeout=190 --graceful-timeout=100 --log-level=DEBUG
+gunicorn core.wsgi:application -b :8000  --workers=5   --timeout=190 --graceful-timeout=100 --log-level=DEBUG
 exec "$@"
