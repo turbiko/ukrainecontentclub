383bef14c79f41083f3dd234ac8cb705521e5746
Andriy Voznyuk <andreyv@ukr.net>
2023-04-02 17:09:22 +0200

    deploy params

--- landing/core/settings/base.py
+++ landing/core/settings/base.py
@@ -168,3 +168,7 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "https://ukrainecontentclub.com.ua/"
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://a1.ukrainecontentclub.com.ua/',
+]
